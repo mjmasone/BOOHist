60c77876f6aeee4ca0e6efbce71bd4b448b7adc1
mjmasone <103435749+mjmasone@users.noreply.github.com>
2026-03-03 10:22:46 -0500

    Initialize main.jsx with React and BooHist component

---
+++ src/main.jsx
@@ -0,0 +1,4 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import BooHist from './BooHist'
+ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><BooHist /></React.StrictMode>)
